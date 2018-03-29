c1acc4b6e244aa60c0a25278728c3c8ac48ae2c0
Daniele Sforza <danielesforza@gmail.com>
2018-03-29 13:17:38 +0200

    Add auto-saving functionality

--- js/app.js
+++ js/app.js
@@ -19,29 +19,13 @@ function shuffle(array) {
 let cards = ['fab fa-html5', 'fab fa-css3-alt', 'fab fa-google', 'fab fa-js', 'fab fa-react', 'fab fa-github', 'fab fa-slack', 'fab fa-stack-overflow', 'fab fa-html5', 'fab fa-css3-alt', 'fab fa-google', 'fab fa-js', 'fab fa-react', 'fab fa-github', 'fab fa-slack', 'fab fa-stack-overflow'];
 
 
-// create a list of open cards
-let openCards = [];
-
-
-// create moves counter
-let moves = 0;
-
-
-// rating in stars
-let rating = 3;
-
-
-// create timer
-let start = 0;
-let timer;
-
-
-// game in progress flag
-let isPlaying = false;
-
-
-// matched pairs counter
-let matched = 0;
+let openCards = [],         // list of open cards
+    moves = 0,              // moves counter
+    rating = 3,             // rating in stars
+    timer,                  // timer
+    seconds = 0,
+    isPlaying = false,      // game in progress flag
+    matched = 0;            // matched pairs counter
 
 
 // initialize new game after loading the DOM
@@ -62,46 +46,55 @@ document.querySelector('.btn-restart').addEventListener('click', newGame);
 
 
 // starts a new game
-function newGame() {
+function newGame(evnt) {
+  // if user resets a game clear saves
+  if (evnt.type === 'click') {
+    localStorage.clear();
+  }
+
   // shuffle the list of cards
   cards = shuffle(cards);
 
-  // game not started yet
-  isPlaying = false;
-
   // reset timer
   clearInterval(timer);
-  timerDisplay.textContent = "00:00";
 
-  // reset moves and stars
-  moves = 0;
+  // set game not started
+  isPlaying = false;
+
+  // if there's not a saved game initialize a new game else load it
+  if (!localStorage.getItem('deck')) {
+    localStorage.setItem('openCards', JSON.stringify([]));  // empty list of open cards
+    localStorage.setItem('moves', 0);                       // reset moves and stars
+    localStorage.setItem('matched', 0);                     // reset matched pairs counter
+    localStorage.setItem('seconds', 0);                     // reset timer counter
+
+    // delete old cards
+    deck.innerHTML = '';
+    // loop through each card and create the HTML
+    let cardFragment = document.createDocumentFragment();
+    for (let i = 0; i < cards.length; i++) {
+      let newCard = document.createElement('li');
+      newCard.id = "card-" + i;
+      newCard.className = "card";
+      newCard.innerHTML = `<i class="${cards[i]}"></i>`;
+      cardFragment.appendChild(newCard);
+    }
+    deck.appendChild(cardFragment);
+  } else {
+    deck.innerHTML = localStorage.getItem('deck');
+  }
+
+  openCards = JSON.parse(localStorage.getItem('openCards'));
+  moves = JSON.parse(localStorage.getItem('moves'));
   setMoves(moves);
-
-  // empty list of open cards
-  openCards = [];
-
-  // reset matched pairs counter
-  matched = 0;
-
-  // delete old cards
-  document.querySelectorAll('.card').forEach(function(card) {
-    card.remove();
-  });
+  matched = JSON.parse(localStorage.getItem('matched'));
+  seconds = JSON.parse(localStorage.getItem('seconds'));
+  gameTimer(true);
 
   // show deck and hide results
   scorePanel.style.display = "block";
   deck.style.display = "flex";
   results.style.display = "none";
-
-  // loop through each card and create the HTML
-  let cardFragment = document.createDocumentFragment();
-  for (let i = 0; i < cards.length; i++) {
-    let newCard = document.createElement('li');
-    newCard.className = "card";
-    newCard.innerHTML = `<i class="${cards[i]}"></i>`;
-    cardFragment.appendChild(newCard);
-  }
-  deck.appendChild(cardFragment);
 
   // set up the event listener for the cards
   deck.addEventListener('click', checkCard);
@@ -115,9 +108,7 @@ function checkCard(evnt) {
   // if it's the first move start the timer
   if ((evnt.target.nodeName === 'LI') && (!evnt.target.classList.contains('open')) && (!evnt.target.classList.contains('match')) && (openCards.length < 2)) {
     openCard(evnt.target);
-    openCards.push(evnt.target);
     if (!isPlaying) {
-      start = Date.now();
       timer = setInterval(gameTimer, 1000);
       isPlaying = true;
     }
@@ -127,10 +118,12 @@ function checkCard(evnt) {
     // NO: hide the cards
     if (openCards.length === 2) {
       setMoves(++moves);
-      if (openCards[0].firstElementChild.className === openCards[1].firstElementChild.className) {
-        lockCards(openCards[0], openCards[1]);
+      let cardOne = document.querySelector('#' + openCards[0]);
+      let cardTwo = document.querySelector('#' + openCards[1]);
+      if (cardOne.firstElementChild.className === cardTwo.firstElementChild.className) {
+        lockCards(cardOne, cardTwo);
       } else {
-        closeCards(openCards[0], openCards[1]);
+        closeCards(cardOne, cardTwo);
       }
     }
   }
@@ -139,6 +132,7 @@ function checkCard(evnt) {
 
 // increment moves counter and check score for stars rating
 function setMoves(counter) {
+  localStorage.setItem('moves', counter);
   document.querySelector('.moves').textContent = counter;
   let stars = document.querySelectorAll('.stars li i');
 
@@ -148,12 +142,12 @@ function setMoves(counter) {
       rating = 3;
     })
   }
-  if (counter === 15) {
+  if (counter >= 15) {
     // stars[2].classList.replace('fas', 'far');    replace() not supported in Edge
     stars[2].className = "far fa-star";
     rating = 2;
   }
-  if (counter === 20) {
+  if (counter >= 20) {
     // stars[1].classList.replace('fas', 'far');    replace() not supported in Edge
     stars[1].className = "far fa-star";
     rating = 1;
@@ -162,16 +156,21 @@ function setMoves(counter) {
 
 
 // display the timer
-function gameTimer() {
-  let now = Date.now() - start;
-  let seconds = Math.floor((now / 1000) % 60).toString();
-  let minutes = Math.floor(now / 1000 / 60).toString();
-  timerDisplay.textContent = minutes.padStart(2, "0") + ":" + seconds.padStart(2, "0");
+function gameTimer(isLoaded) {
+  // if I'm not loading a game then increment seconds, this is done for not adding 1 second after loading a game
+  if (isLoaded === undefined) {
+      seconds++;
+      localStorage.setItem('seconds', seconds);
+  }
+  let secs = (seconds % 60).toString();
+  let mins = Math.floor(seconds / 60).toString();
+  timerDisplay.textContent = mins.padStart(2, "0") + ":" + secs.padStart(2, "0");
 }
 
 
 // display the card's symbol
 function openCard(card) {
+  openCards.push(card.id);
   card.classList.add('open');
 }
 
@@ -186,7 +185,8 @@ function closeCards(cardOne, cardTwo) {
   setTimeout(function() {
     cardOne.classList.remove('close');
     cardTwo.classList.remove('close');
-    openCards = []; // reset open cards list
+    openCards = [];     // reset open cards list
+    saveGameState();
   }, 1500);
 }
 
@@ -195,10 +195,12 @@ function closeCards(cardOne, cardTwo) {
 function lockCards(cardOne, cardTwo) {
   cardOne.className = 'card match';
   cardTwo.className = 'card match';
-  openCards = []; // reset open cards list
+  openCards = [];       // reset open cards list
   matched++;
+  saveGameState();
   if (matched === 8) {
     clearInterval(timer);
+    localStorage.clear();
     setTimeout(function() {
       showResults();
     }, 500)
@@ -223,6 +225,14 @@ function showResults() {
     <td>Time</td>
     <td>${timerDisplay.textContent}</td>
   </tr>`;
+}
+
+
+// save game state in local storage for later reloading when visiting the page
+function saveGameState() {
+  localStorage.setItem('deck', deck.innerHTML);
+  localStorage.setItem('openCards', JSON.stringify(openCards));
+  localStorage.setItem('matched', matched);
 }
 
 
