2a3933f64e4bdd5988864ec2e30f24b20bbdc6e1
Daniele Sforza <danielesforza@gmail.com>
2018-03-30 12:10:05 +0200

    feat: Add save score to a local leaderboard

--- README.md
+++ README.md
@@ -1,20 +1,52 @@
-# Memory Game Project
+# Memory Game
 
-## Table of Contents
+### Instructions
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+The game board consists of sixteen "cards" arranged in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. The cards are arranged randomly on the grid with the symbol face down. The gameplay rules are very simple: flip over two hidden cards at a time to locate the ones that match!
 
-## Instructions
+Each turn:
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+- The player flips one card over to reveal its underlying symbol.
+- The player then turns over a second card, trying to find the corresponding card with the same symbol.
+- If the cards match, both cards stay flipped over.
+- If the cards do not match, both cards are flipped face down.
 
-To get started, open `js/app.js` and start building out the app's functionality
+The game ends once all cards have been correctly matched.
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+------
 
-## Contributing
+### Scores
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+Player performance is tracked with number of moves, a timer for the game duration and a star rating:
 
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+- 3 stars - less than 15 moves
+- 2 stars - between 15 and 20 moves
+- 1 star - more than 20 moves
+
+At the end of the game, the player gets a message with the game statistics and the choice to save the score in a local leaderboard. After saving the leaderboard is shown.
+
+Near the timer there's a restart button to shuffle the cards and reset scores.
+
+------
+
+### Saves
+
+During the game, the state of the deck and the scores are saved in the browser to be reloaded the next time the page is opened.
+
+The leaderboard is also saved locally in the browser and it displays the best 5 scores.
+
+------
+
+### Requirements
+
+This game works in all modern desktop and mobile browsers, access to local storage is required.
+
+[Font Awesome](https://fontawesome.com/) icons and [Google Fonts](https://fonts.google.com/) used in the project are loaded directly from the page.
+
+Background image from [Subtle Patterns](https://www.toptal.com/designers/subtlepatterns/) is included.
+
+##### Known issues
+
+When loading locally in Microsoft Edge the project can't access local storage and breaks the page. Loading from a web server works as intended.
+
+Next version will add a check to skip local storage if not supported by the browser.
--- js/app.js
+++ js/app.js
@@ -19,8 +19,12 @@ function shuffle(array) {
 const scorePanel = document.querySelector('.score-panel');
 const deck = document.querySelector('.deck');
 const results = document.querySelector('.results');
+const stats = document.querySelector('.stats');
 const score = document.querySelector('.score');
+const savePanel = document.querySelector('.save-score');
 const timerDisplay = document.querySelector('.timer');
+const leaderboardPanel = document.querySelector('.leaderboard');
+const leaderboardTable = document.querySelector('.leaderboard-table');
 
 
 let cards = [
@@ -47,7 +51,8 @@ let cards = [
     timer,                  // timer
     seconds = 0,
     isPlaying = false,      // game in progress flag
-    matched = 0;            // matched pairs counter
+    matched = 0,            // matched pairs counter
+    leaderboard = [];
 
 
 // initialize new game after loading the DOM
@@ -57,13 +62,14 @@ document.addEventListener('DOMContentLoaded', newGame);
 // set reset buttons
 document.querySelector('.restart').addEventListener('click', newGame);
 document.querySelector('.btn-restart').addEventListener('click', newGame);
+document.querySelector('.btn-save').addEventListener('click', saveScore);
 
 
 // starts a new game
 function newGame(evnt) {
   // if user resets a game clear saves
   if (evnt.type === 'click') {
-    localStorage.clear();
+    deleteGameState();
   }
 
   // shuffle the list of cards
@@ -104,11 +110,15 @@ function newGame(evnt) {
   matched = JSON.parse(localStorage.getItem('matched'));
   seconds = JSON.parse(localStorage.getItem('seconds'));
   gameTimer(true);
+  localStorage.getItem('leaderboard') ? leaderboard = JSON.parse(localStorage.getItem('leaderboard')) : localStorage.setItem('leaderboard', JSON.stringify([]));
 
   // show deck and hide results
   scorePanel.style.display = 'block';
   deck.style.display = 'flex';
   results.style.display = 'none';
+  stats.style.display = 'block';
+  savePanel.style.display = 'block';
+  leaderboardPanel.style.display = 'none';
 
   // set up the event listener for the cards
   deck.addEventListener('click', checkCard);
@@ -158,11 +168,13 @@ function setMoves(counter) {
   }
   if (counter >= 15) {
     // stars[2].classList.replace('fas', 'far');    replace() not supported in Edge
+    // I commented on https://github.com/mdn/browser-compat-data/pull/1121#issuecomment-376607876 to issue a change in the MDN compatibility table
     stars[2].className = 'far fa-star';
     rating = 2;
   }
   if (counter >= 20) {
     // stars[1].classList.replace('fas', 'far');    replace() not supported in Edge
+    // I commented on https://github.com/mdn/browser-compat-data/pull/1121#issuecomment-376607876 to issue a change in the MDN compatibility table
     stars[1].className = 'far fa-star';
     rating = 1;
   }
@@ -176,9 +188,15 @@ function gameTimer(isLoaded) {
       seconds++;
       localStorage.setItem('seconds', seconds);
   }
-  let secs = (seconds % 60).toString();
-  let mins = Math.floor(seconds / 60).toString();
-  timerDisplay.textContent = mins.padStart(2, '0') + ':' + secs.padStart(2, '0');
+  timerDisplay.textContent = secsToMinSec(seconds);
+}
+
+
+// given a number of seconds return a string in the form of mm:ss
+function secsToMinSec(x) {
+  let secs = (x % 60).toString();
+  let mins = Math.floor(x / 60).toString();
+  return mins.padStart(2, '0') + ':' + secs.padStart(2, '0');
 }
 
 
@@ -214,7 +232,7 @@ function lockCards(cardOne, cardTwo) {
   saveGameState();
   if (matched === 8) {
     clearInterval(timer);
-    localStorage.clear();
+    deleteGameState();
     setTimeout(function() {
       showResults();
     }, 500)
@@ -241,12 +259,67 @@ function showResults() {
   </tr>`;
 }
 
+function saveScore(evnt) {
+  // validate input
+  let name = (document.querySelector('.name').value).trim();
+  if (name === '') {
+    alert('Insert a name');
+    return;
+  }
+  // save score in the leaderboard and sort the leaderboard by moves and time
+  let score = {name:name, rating:rating, moves:moves, time:seconds};
+  leaderboard.push(score);
+  leaderboard.sort(function(a, b) {
+    return (a.moves === b.moves) ? (a.time - b.time) : (a.moves - b.moves);
+  });
+  localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
+
+  // hide stats and save input
+  savePanel.style.display = 'none';
+  stats.style.display = 'none';
+
+  // create leaderboard table
+  leaderboardTable.innerHTML = '';
+  let scoreFragment = document.createDocumentFragment();
+  let firstRow = document.createElement('tr');
+  firstRow.innerHTML = `<td>Name</td>
+  <td>Rating</td>
+  <td>Moves</td>
+  <td>Time</td>`;
+  scoreFragment.appendChild(firstRow);
+  for (let i = 0; i <= leaderboard.length - 1; i++) {
+    if (i === 5) {
+      break;
+    }
+    let newRow = document.createElement('tr');
+    newRow.innerHTML = `<td>${leaderboard[i].name}</td>
+    <td>${leaderboard[i].rating} <i class="fas fa-star"></i></td>
+    <td>${leaderboard[i].moves}</td>
+    <td>${secsToMinSec(leaderboard[i].time)}</td>`;
+    scoreFragment.appendChild(newRow);
+  }
+  leaderboardTable.appendChild(scoreFragment);
+
+  // display leaderboard
+  leaderboardPanel.style.display = 'block';
+}
+
 
 // save game state in local storage for later reloading when visiting the page
 function saveGameState() {
   localStorage.setItem('deck', deck.innerHTML);
   localStorage.setItem('openCards', JSON.stringify(openCards));
   localStorage.setItem('matched', matched);
+}
+
+
+// delete game state
+function deleteGameState() {
+  localStorage.removeItem('openCards');
+  localStorage.removeItem('matched');
+  localStorage.removeItem('moves');
+  localStorage.removeItem('seconds');
+  localStorage.removeItem('deck');
 }
 
 
